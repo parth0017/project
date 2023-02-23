101881ed8176d24f1c8957e04d2db4153e9782b9
Parth Lathiya <86923460+parth0017@users.noreply.github.com>
2023-02-23 22:10:27 +0530

    Add files via upload

---
+++ admin.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>login form</title>
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+   
+<div class="form-container">
+   <div class="background">
+      <img src="css/home.jpg" alt="home" >
+   </div>
+   <form action="" method="post">
+      <h3>login now</h3>
+      <input type="id" name="id" required placeholder="id number">
+      <input type="password" name="password" required placeholder="enter your password">
+      <input type="submit" name="submit" value="login now" class="form-btn">
+   </form>
+
+</div>
+
+</body>
+</html>
---
+++ login_form.php
@@ -0,0 +1,29 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>login form</title>
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+<div class="hero">
+<div class="form-container">
+   <form action="" method="post">
+      <h3>login now</h3>
+      <input type="email" name="email" required placeholder="enter your email">
+      <input type="password" name="password" required placeholder="enter your password">
+      <input type="submit" name="submit" value="login now" class="form-btn">
+      <p>Don't have an account? <a href="register_form.php">register now</a></p>
+      <p><a href="admin.php">Admin Login</a></p>
+   </form>
+</div>
+</div>
+
+</body>
+</html>
